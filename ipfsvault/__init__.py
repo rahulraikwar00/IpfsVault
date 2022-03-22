500f088f2531c1809093a34d75b904d77b63fe24
rahul <rr200636@gmail.com>
2022-03-22 13:28:33 +0530

    init ipfsvault basic

---
+++ ipfsvault/__init__.py
@@ -0,0 +1,10 @@
+"""
+
+Ipfsvault
+
+decentralize storage for authenticatino process package it uses ipfs for storing the data and multihash, md5, sha256, sha512 for authentication.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Rahul Raikwar'
+__credits__ = 'Argonne National Laboratory'
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='ipfsvault',
+    version='0.0.1',
+    description=
+    "decentralize storage for authenticatino process package it uses ipfs for storing the data and multihash, md5, sha256, sha512 for authentication",
+    # url='https://github.com/shuds13/pyexample',
+    author='Rahul Raikwar',
+    author_email='rr200636@gmail.com',
+    license='BSD 2-clause',
+    packages=['ipfsvault'],
+    install_requires=[
+        'mpi4py>=2.0',
+        'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
