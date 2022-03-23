d4a9f7b94e3a0a8edb773b44669539da673e9c5d
rahul <rr200636@gmail.com>
2022-03-23 12:29:12 +0530

    temp requirment.txt

--- Engine/card.py
+++ Engine/card.py
@@ -45,22 +45,22 @@ class Card:
         return data[offset:]
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
 
-#     key = "7195bb5c30deff29d83ac52b570b8c65"
-#     details = {
-#         'aadhaarno': '234-2788-98987',
-#         'name': 'mohd shibli',
-#         'dob': '27-09-1997',
-#         "address": "kathmandu",
-#         'email': 'mohdshitbit@kamfakd.com'
-#     }
-#     card = Card(key.encode("utf-8"), str(details))
-#     # print(card)
-#     cipher = card.encrypt_data
+    key = "7195bb5b30deff29d83ac5n2b50b8c68"
+    details = {
+        'aadhaarno': '234-2788-98987',
+        'name': 'mohd shibli',
+        'dob': '27-09-1997',
+        "address": "kathmandu",
+        'email': 'mohdshitbit@kamfakd.com'
+    }
+    card = Card(key.encode("utf-8"), str(details))
+    # print(card)
+    cipher = "53efd37873e384a846aea88270afd6a5c57a6a2c175a6d480113ad5b293e5d79729e2b5b900a0cf611eebce0b151d64804d6314abb39ae887b51e939aa02cf4bb85f373400c18deb2d14cbe52ce3ba13f7c7d5a5eca5c5e7270e07c2c0f64f225fcae13792d3030a0dbb05415a13f6602981f6381691c9cc8eca34c1610bb1d920a88177"
 
-#     # size of the ciphertext in kb
-#     # print(len(cipher) / 1024)
-#     # print(cipher)
-#     print("-------------------------------------------")
-#     print(Card(key.encode("utf-8")).decrypt_data(cipher))
+    # size of the ciphertext in kb
+    # print(len(cipher) / 1024)
+    print(cipher)
+    print("-------------------------------------------")
+    print(Card(key.encode("utf-8")).decrypt_data(cipher))
--- vaut.py
+++ vaut.py
@@ -1,3 +1,6 @@
+from ast import Str
+from base64 import encode
+from codecs import EncodedFile
 import os
 import sys
 import json
@@ -7,24 +10,27 @@ import cryptography
 
 
 class Valut:
-    def __init__(self) -> None:
-        self.key = ""
+    def __init__(self, masterkey: Str) -> None:
+        self.masterkey = masterkey
         self.aadharno = ""
+        self.encodefile = ""
         # create a json file and return file object if not exist
         if not os.path.exists("data.json"):
             with open("data.json", "w") as f:
                 json.dump([], f)
 
     # encrypt the data and return the ciphertext
-    def encryptfile(self, details: str):
+    def encryptfile(self, details: dict):
         card = cardX.Card(self.key.encode("utf-8"), str(details))
         cipher = card.encrypt_data
         return cipher
 
     # decrypt the data and return the plaintext
-    def decryptfile(self, details):
-        print(details)
-        return cardX.Card(self.key.encode("utf-8")).decrypt_data(details)
+    def decryptfile(self, datastring: str):
+        print(len(datastring))
+        card = cardX.Card(self.key.encode("utf-8")).decrypt_data(datastring)
+        print(card)
+        return True
 
     def upload(self, details: dict):
         # encrypt the details
@@ -33,26 +39,17 @@ class Valut:
         response = requests.post("http://127.0.0.1:5001/api/v0/add", files=files)
         p = response.json()
         # hash = p["Hash"]
+        # print(p)
 
         return p
 
     def download(self, hash: str):
-        # with open("data.json", "r") as f:
-        #     data = json.load(f)
-        #     self.aadharno = data[0][aadhaarno]
 
         params = (("arg", hash),)
         response_two = requests.post(
             "http://127.0.0.1:5001/api/v0/block/get", params=params
         )
         return response_two.text
-
-        # with open("data.json", "r") as f:
-        #     data = json.load(f)
-        # if aadhaarno in data:
-        #     return self.decryptfile(data[aadhaarno])
-        # else:
-        #     return False
 
     def setkey(self, key: str):
         self.key = key
@@ -63,14 +60,14 @@ obj = Valut()
 obj.key = "7195bb5b30deff29d83ac5n2b50b8c68"
 data = obj.upload(
     {
-        "aadhaarno": "334-788-98188",
-        "name": "karan malviye",
-        "dob": "27-09-2000",
-        "address": "bhopal ",
-        "email": "karnamalviye@gmail.com",
+        "aadhaarno": "333-788-98188",
+        "name": "Rahul Raikwar",
+        "dob": "27-09-1997",
+        "address": "kathmandu",
+        "email": ")",
     }
 )
-edata = obj.download(data["Hash"])
-print(edata)
-
-print(obj.decryptfile(edata))
+sdata = obj.download(data["Hash"])
+print(len(sdata))
+obj.encodefile = sdata
+obj.decryptfile()
